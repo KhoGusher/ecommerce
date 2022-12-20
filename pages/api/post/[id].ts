6d270d580fc500b4e97b39cf8a88d0783c46505e
KhoGusher <kondwaninantchito@gmail.com>
2022-12-20 10:05:56 +0200

    auth callback fixed

---
+++ pages/api/post/[id].ts
@@ -0,0 +1,19 @@
+// pages/api/post/[id].ts
+
+import { Post } from '.prisma/client';
+import prisma from '../../../lib/prisma';
+
+// DELETE /api/post/:id
+export default async function handle(req: { query: { id: any; }; method: string; }, res: { json: (arg0: Post) => void; }) {
+  const postId = req.query.id;
+  if (req.method === 'DELETE') {
+    const post = await prisma.post.delete({
+      where: { id: postId },
+    });
+    res.json(post);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`,
+    );
+  }
+}
